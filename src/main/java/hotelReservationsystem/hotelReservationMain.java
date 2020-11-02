e49fffae61fe159f48f7e1017dcf2e9b82c59bda
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-11-02 17:31:17 +0530

    STARTED:Hotel Reservation System

---
+++ src/main/java/hotelReservationsystem/hotelReservationMain.java
@@ -0,0 +1,7 @@
+package hotelReservationsystem;
+
+public class hotelReservationMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+    }
+}
